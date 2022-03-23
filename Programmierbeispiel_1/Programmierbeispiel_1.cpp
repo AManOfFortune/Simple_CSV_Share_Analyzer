d0d32cb3c671872df11df71003d7a9c3ba19484a
Samuel Muskovich <samuel.muskovich@gmail.com>
2022-03-23 17:37:20 +0100

    Added temporary hash function, added search function

--- Programmierbeispiel_1/Programmierbeispiel_1.cpp
+++ Programmierbeispiel_1/Programmierbeispiel_1.cpp
@@ -2,7 +2,11 @@
 #include <fstream>
 #include <string>
 
-#define hashTableSize 10
+#define HASH_TABLE_SIZE 2003
+
+#define RED_BG "\033[41m"
+#define GREEN_BG "\033[42m"
+#define RESET "\033[m"
 
 //Struct containing all numerical information about one day of a certain share
 struct kursdaten {
@@ -24,18 +28,19 @@ struct share {
     struct kursdaten kursdaten[30];
 };
 
-//Struct containing only kürzel
+//Struct containing kürzel & name
 //Necessary so we can hash the name and arrive at the corresponding kürzel
 //IS IMPROVEABLE, MAYBE WE CAN ONLY SAVE THE INDEX OF THE DATA IN OTHER ARRAY INSTEAD TO IMPROVE ACCESS SPEED
 struct mapNameToKuerzel {
+    std::string name;
     std::string kuerzel;
 };
 
 //Deletes all structs from heap
-void clearHeap(struct share* kuerzelHashTable[10], struct mapNameToKuerzel* nameHashTable[10]) {
+void clearHeap(struct share* kuerzelHashTable[HASH_TABLE_SIZE], struct mapNameToKuerzel* nameHashTable[HASH_TABLE_SIZE]) {
 
     //Loops arrays once, deletes elements if they are not NULL
-    for (int i = 0; i < hashTableSize; i++) {
+    for (int i = 0; i < HASH_TABLE_SIZE; i++) {
 
         if(kuerzelHashTable[i] != NULL)
             delete kuerzelHashTable[i];
@@ -43,6 +48,19 @@ void clearHeap(struct share* kuerzelHashTable[10], struct mapNameToKuerzel* name
         if(nameHashTable[i] != NULL)
             delete nameHashTable[i];
     }
+}
+
+//TEMPORARY HASH FUNCTION
+int hashFunc(std::string str) {
+    //Get ASCII values of string
+    int sum = 0;
+    for (std::string::size_type i = 0; i < str.size(); ++i) {
+        sum = sum + int(str[i]);
+    }
+    //-1, because array has on last index '\0'
+    int hashIndex = (sum * (HASH_TABLE_SIZE / 360)) % HASH_TABLE_SIZE;
+
+    return hashIndex;
 }
 
 //Imports share-data from file, saves it in share-struct
@@ -121,14 +139,15 @@ void importData(std::string fileName, struct share* associatedShare) {
 }
 
 //DEBUGGING FUNCTION, DELETE LATER
-//Prints all general information about all shares in array
-void printAllShareData(struct share* kuerzelHashTable[hashTableSize])
+//Prints all information about all shares in hash table
+void printAllShareData(struct share* kuerzelHashTable[HASH_TABLE_SIZE])
 {
     system("cls");
 
-    for (int i = 0; i < hashTableSize; i++) {
+    for (int i = 0; i < HASH_TABLE_SIZE; i++) {
 
         if (kuerzelHashTable[i] != NULL) {
+            std::cout << "Index: " << i << std::endl;
             std::cout << "Name: " << kuerzelHashTable[i]->name << std::endl;
             std::cout << "Kuerzel: " << kuerzelHashTable[i]->kuerzel << std::endl;
             std::cout << "WKN: " << kuerzelHashTable[i]->wkn << std::endl;
@@ -147,7 +166,7 @@ void printAllShareData(struct share* kuerzelHashTable[hashTableSize])
 }
 
 //Adds a new share to the arrays
-void add(struct share* kuerzelHashTable[hashTableSize], struct mapNameToKuerzel* nameHashTable[10])
+void add(struct share* kuerzelHashTable[HASH_TABLE_SIZE], struct mapNameToKuerzel* nameHashTable[HASH_TABLE_SIZE])
 {
     system("cls");
 
@@ -164,6 +183,7 @@ void add(struct share* kuerzelHashTable[hashTableSize], struct mapNameToKuerzel*
     std::cout << "Enter kuerzel: ";
     std::cin >> kuerzel;
 
+    //Initializes and fills the share and map structs
     struct share* newShare = new struct share();
     struct mapNameToKuerzel* newMap = new struct mapNameToKuerzel();
 
@@ -172,26 +192,104 @@ void add(struct share* kuerzelHashTable[hashTableSize], struct mapNameToKuerzel*
     newShare->kuerzel = kuerzel;
 
     newMap->kuerzel = kuerzel;
+    newMap->name = name;
 
+    //Imports (and saves) the data to the newShare struct
     importData(kuerzel + ".csv", newShare);
 
-    //CHANGES ONCE HASH FUNCTION IS DONE, WORKS FOR NOW
-    for (int i = 0; i < hashTableSize; i++) {
-        if (kuerzelHashTable[i] == NULL && nameHashTable[i] == NULL) {
-            kuerzelHashTable[i] = newShare;
-            nameHashTable[i] = newMap;
-            break;
+    //Saves both structs inside their respective hash tables
+    kuerzelHashTable[hashFunc(kuerzel)] = newShare;
+    nameHashTable[hashFunc(name)] = newMap;
+
+    //DEBUGGING ONLY, REMOVE BEFORE FINAL SUBMIT
+    printAllShareData(kuerzelHashTable);
+}
+
+//Searches for either a name or a kuerzel in the corresponding hash table, returns NULL if nothing is found
+struct share* search(struct share* kuerzelHashTable[HASH_TABLE_SIZE], struct mapNameToKuerzel* nameHashTable[HASH_TABLE_SIZE])
+{
+    //Asks for the input mode
+    std::cout << "Input (n)ame or (k)uerzel?" << std::endl;
+    char mode = ' ';
+    do {
+        std::cout << "Choose: ";
+        std::cin >> mode;
+    } while (mode != 'n' && mode != 'k');
+
+    //Gets initialized differently depending on input mode
+    std::string kuerzel;
+
+    //If user wants to input a name, ask it, look for it and save the associated kuerzel if it is found
+    if (mode == 'n') {
+        //Ask the user for the name
+        std::string name;
+        do {
+            std::cout << "Name: ";
+            std::cin >> name;
+        } while (name.length() <= 0);
+
+        //Search for that name in the hash table
+        struct mapNameToKuerzel* result = nameHashTable[hashFunc(name)];
+
+        //If result is NULL immediately, return NULL
+        if (result == NULL)
+            return NULL;
+        //Otherwise check if result is correct, if it is not, do cubic jumping
+        else if (result->name != name) {
+            //INSERT CUBIC JUMPING HERE
         }
+
+        kuerzel = result->kuerzel;
+    }
+    //Otherwise simply ask the user to input the kuerzel
+    else {
+        //Ask the user for the kuerzel
+        do {
+            std::cout << "Kuerzel: ";
+            std::cin >> kuerzel;
+        } while (kuerzel.length() <= 0);
     }
 
-    printAllShareData(kuerzelHashTable);
+    //Search for the kuerzel in the hash table
+    struct share* result = kuerzelHashTable[hashFunc(kuerzel)];
+
+    //If result is NULL immediately, return NULL
+    if (result == NULL)
+        return NULL;
+    //Otherwise check if result is correct, if it is not, do cubic jumping
+    else if (result->kuerzel != kuerzel) {
+        //INSERT CUBIC JUMPING HERE
+    }
+
+    //Return the result
+    return result;
+}
+
+//Displays the search results
+void displaySearchResults(struct share* result) 
+{
+    system("cls");
+
+    if (result != NULL) {
+        std::cout << "Name: " << result->name << std::endl;
+        std::cout << "Kuerzel: " << result->kuerzel << std::endl;
+        std::cout << "WKN: " << result->wkn << std::endl;
+        std::cout << "Data: " << std::endl;
+
+        struct kursdaten entry = result->kursdaten[0];
+        std::cout << entry.date << ": " << entry.open << " | " << entry.high << " | " << entry.low << " | " << entry.close << " | " << entry.adjClose << " | " << entry.volume << "\n" << std::endl;
+    }
+    else std::cout << "Entry not found!\n" << std::endl;
+
+    system("pause");
+    system("cls");
 }
 
 int main()
 {
     //Initializes both hash tables - One is hashed with the kürzel, the other one with the name
-    struct share* kuerzelHashTable[hashTableSize]{ NULL };
-    struct mapNameToKuerzel* nameHashTable[hashTableSize]{ NULL };
+    struct share* kuerzelHashTable[HASH_TABLE_SIZE]{ NULL };
+    struct mapNameToKuerzel* nameHashTable[HASH_TABLE_SIZE]{ NULL };
 
     //Flag to stop looping if user wants to exit
     bool loop = true;
@@ -216,6 +314,7 @@ int main()
         case 2:
             break;
         case 3:
+            displaySearchResults(search(kuerzelHashTable, nameHashTable));
             break;
         case 4:
             break;
@@ -224,9 +323,11 @@ int main()
         case 6:
             break;
         case 7:
-            clearHeap(kuerzelHashTable, nameHashTable);
             loop = false;
             break;
         }
     }
+
+    clearHeap(kuerzelHashTable, nameHashTable);
+    return 0;
 }
