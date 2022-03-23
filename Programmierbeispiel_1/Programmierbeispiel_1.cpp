53aade68ea2e1758dee0b5b99214ef1b2c71cd84
Samuel Muskovich <samuel.muskovich@gmail.com>
2022-03-23 23:21:54 +0100

    Added plot function

--- Programmierbeispiel_1/Programmierbeispiel_1.cpp
+++ Programmierbeispiel_1/Programmierbeispiel_1.cpp
@@ -334,6 +334,129 @@ void deleteEntry(struct share* entryToDelete, struct share* kuerzelHashTable[HAS
     system("pause");
 }
 
+//Helper function for plot
+//Returns the digits of a given number
+int getDigits(int number) {
+    int digits = 0;
+
+    do {
+        number /= 10;
+        digits++;
+    } while (number != 0);
+
+    return digits;
+}
+
+//Prints a simple candlestick chart for a single share
+void plot(struct share* shareToPlot)
+{
+    system("cls");
+
+    if (shareToPlot != NULL) {
+        std::cout << "Name: " << shareToPlot->name << std::endl;
+        std::cout << "Kuerzel: " << shareToPlot->kuerzel << std::endl;
+        std::cout << "WKN: " << shareToPlot->wkn << std::endl;
+
+        //First, save what are the highest and lowest values of high/low
+        float highest = 0;
+        float lowest = 0;
+        for (int i = 0; i < 30; i++) {
+            struct kursdaten day = shareToPlot->kursdaten[i];
+
+            //On the first day, properly initialize highest and lowest
+            if (i == 0) {
+                lowest = day.low;
+                highest = day.high;
+            }
+
+            //On all other days, save any value that was higher/lower than the current highest/lowest
+            if (day.high > highest)
+                highest = day.high;
+            if (day.low < lowest)
+                lowest = day.low;
+        }
+
+        //Next, we can calculate the top and bottom boundaries of the chart
+        //This is the space we will have between the border and the highest/lowest points of our chart
+        //Currently, we set that space to be 1% on each side
+        float topBoundary = static_cast<float>(highest * 1.01);
+        float bottomBoundary = static_cast<float>(lowest * 0.99);
+
+        //We also need to know how many digits each of those numbers has to print them properly later
+        int topBoundaryDigits = getDigits(static_cast<int>(topBoundary));
+        int bottomBoundaryDigits = getDigits(static_cast<int>(bottomBoundary));
+
+        //Now we specify the width (in characters) the chart takes up on our screen
+        int chartWidth = 100;
+
+        //With all those calculations done, we can start printing our chart!
+        //First, we print a line with our top and bottom boundary numbers
+        std::cout << "             " << static_cast<int>(bottomBoundary);
+        for (int i = 0; i < chartWidth - bottomBoundaryDigits - topBoundaryDigits; i++) {
+            std::cout << " ";
+        }
+        std::cout << static_cast<int>(topBoundary) << std::endl;
+
+        //Then we print the top line of the chart area
+        std::cout << "            +";
+        for (int i = 0; i < chartWidth; i++) {
+            std::cout << "-";
+        }
+        std::cout << "+" << std::endl;
+
+        //Next, we print the actual data
+        for (int i = 0; i < 30; i++) {
+            struct kursdaten day = shareToPlot->kursdaten[i];
+
+            //Change the color of the bar depending if the close was higher than the low
+            bool green = day.close > day.open ? true : false;
+
+            //We need to scale the open/close value of the day with the chart width & its boundaries to find the starting/ending characters
+            int openScaled = static_cast<int>((day.open - bottomBoundary) * chartWidth / (topBoundary - bottomBoundary));
+            int closeScaled = static_cast<int>((day.close - bottomBoundary) * chartWidth / (topBoundary - bottomBoundary));
+
+            //Same with the high/low
+            int highScaled = static_cast<int>((day.high - bottomBoundary) * chartWidth / (topBoundary - bottomBoundary));
+            int lowScaled = static_cast<int>((day.low - bottomBoundary) * chartWidth / (topBoundary - bottomBoundary));
+
+            //Now we can start printing by printing the date and a seperator
+            std::cout << day.date << "  |";
+
+            //Next we print characters for the whole chart width
+            for (int width = 0; width < chartWidth; width++) {
+                //If close is higher than open and we are between the two, make background green
+                if (green && width >= openScaled && width <= closeScaled)
+                    std::cout << GREEN_BG;
+                //If open is higher than close and we are between the two, make background red
+                else if (!green && width <= openScaled && width >= closeScaled)
+                    std::cout << RED_BG;
+
+                //If we are between the high and low, print a dash, else print a space
+                if (width >= lowScaled && width <= highScaled)
+                    std::cout << "-";
+                else
+                    std::cout << " ";
+
+                //Reset the colors (does not do anything if we have no colors)
+                std::cout << RESET;
+            }
+            //Print a closing seperator at the end
+            std::cout << "|" << std::endl;
+        }
+
+        //Lastly, we print the closing line
+        std::cout << "            +";
+        for (int i = 0; i < chartWidth; i++) {
+            std::cout << "-";
+        }
+        std::cout << "+\n" << std::endl;
+    }
+    else std::cout << "Share not found!\n" << std::endl;
+
+    system("pause");
+    system("cls");
+}
+
 int main()
 {
     //Initializes both hash tables - One is hashed with the kürzel, the other one with the name
@@ -367,6 +490,7 @@ int main()
                 displaySearchResults(search(kuerzelHashTable, nameHashTable));
                 break;
             case 4:
+                plot(search(kuerzelHashTable, nameHashTable));
                 break;
             case 5:
                 break;
